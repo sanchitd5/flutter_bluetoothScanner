6776b77c60e78d9000998ec16c3a5125f783bfdd
sanchitd5 <sanchitd5@live.com>
2020-02-26 14:27:50 +1100

    Log DIO construction

--- lib/helpers/API/dioInstance.dart
+++ lib/helpers/API/dioInstance.dart
@@ -1,13 +1,18 @@
 import 'package:dio/dio.dart';
 
 import '../../configurations/configurations.dart';
+import '../../utils/utils.dart';
 
 class DioInstance {
   final Dio _instance = new Dio();
+  final String _backendUrl = Configurations().backendUrl;
+
   DioInstance() {
-    _instance.options.baseUrl = Configurations().backendUrl + '/api/';
-    _instance.options.connectTimeout=5000;
-    _instance.options.receiveTimeout=3000;
+    _instance.options.baseUrl = _backendUrl;
+    logger
+        .i('DIO instance Constructed\nBackend Url: ' + _backendUrl.toString());
+    _instance.options.connectTimeout = 5000;
+    _instance.options.receiveTimeout = 3000;
   }
 
   Dio get construct {
