8b2cdbaf3826ec5b107ec59778cdd8299cb48d2e
Sanchit Dang <sanchitd5@live.com>
2020-03-03 14:38:46 +1100

    flutter/alarmManagerBootstraper: Code Cleanup

--- lib/utils/AndroidAlarmBootstrapper/AlarmManagerBootstraper.dart
+++ lib/utils/AndroidAlarmBootstrapper/AlarmManagerBootstraper.dart
@@ -1,71 +1,32 @@
 import 'package:android_alarm_manager/android_alarm_manager.dart';
-import 'package:bluetoothScanner/models/modelHelpers.dart';
 import 'package:flutter/foundation.dart';
-import 'package:location/location.dart';
 
 import '../utils.dart';
-import '../../helpers/helpers.dart';
 
 class AlarmManagerModel {
   final String name;
   final Function function;
   final Duration interval;
+
   AlarmManagerModel(
       {@required this.name, @required this.function, @required this.interval});
 }
 
-void testFunction() async {
-  final DateTime now = DateTime.now();
-  Location _location = new Location();
-
-  print("[$now] Hello, world!");
-  logger.i('SERVICE CALLED');
-  print('SERVICE CALLED');
-  PermissionStatus locationPermissionCheck =
-      await _location.requestPermission();
-  if (locationPermissionCheck == PermissionStatus.GRANTED) {
-    LocationData _xyz = await _location.getLocation();
-    logger.i(_xyz.latitude);
-    API _api = new API();
-    DIOResponseBody response = await _api.sendWeatherPredicationData(
-        latitude: _xyz.latitude, longitude: _xyz.longitude);
-    if (response.success) {
-      logger.i('response sent');
-      print('response sent');
-    } else {
-      logger.i('error from api');
-      print('error from api');
-    }
-  }
-}
-
-class AlarmManagerBootstraper {
+class AlarmManagerBootstrapper {
   static int counter = 0;
-  static List<AlarmManagerModel> services = [
-    AlarmManagerModel(
-        function: testFunction, interval: Duration(seconds: 2), name: 'bla'),
-  ];
+  static List<AlarmManagerModel> services = [];
 
   static void bootstrap() async {
     logger.i('Bootstrapping Alarm Manager');
     await AndroidAlarmManager.initialize();
-    await AndroidAlarmManager.cancel(0);
-    await AndroidAlarmManager.cancel(1);
     services.forEach((f) async {
-      // bool status =
-      //     await AndroidAlarmManager.periodic(f.interval, counter, f.function);
-      // if (status) {
-      //   logger.i('${f.name} Started in Alarm Manager');
-      //   counter = counter + 1;
-      // } else {
-      //   logger.e('Error Starting Alarm Manager Service');
-      // }
-      try {
-        await AndroidAlarmManager.periodic(f.interval, counter, f.function);
-        logger.i(
-            '${f.name} has been pushed to isolate queue and ID: ${counter.toString()} has been assigned to it.');
-      } catch (e) {
-        print(e);
+      bool status =
+          await AndroidAlarmManager.periodic(f.interval, counter, f.function);
+      if (status) {
+        logger.i('${f.name} Started in Alarm Manager');
+        counter = counter + 1;
+      } else {
+        logger.e('Error Starting Alarm Manager Service');
       }
     });
   }
